dd37c8e126f1fd7f4ada69ce4da8b97e435ad863
Manan Meratwal <mananmeratwal5@gmail.comgit config --global user.email mananmeratwal6@gmail.com>
2024-07-20 11:16:22 +0530

    ContextApi

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import ContextProvider from './Day-Night-Theme-Context/ContextProvider';
 import MovieProvider from './DynamicContext/MovieProvider';
 import MovieList from './DynamicContext/MovieList';
+import './App.css'
 
 const App = () => {
   return (
     <div>
 
-      <h1 style={{background:"yellow"}}>Here is the example of ContextApi, I have used to make Dark and Light mode Theme</h1>
+      <h1 style={{background:"blue"}}>Here is the example of ContextApi, I have used to make Dark and Light mode Theme</h1>
       <ContextProvider/>
       <br/>
       <br/>
       <br/>
-      <h1 style={{background:"yellow"}}>Here is the example of Dynamic context, I send the the movie data from MovieContext to MovieList, and render the Data's </h1>
+      <h1 style={{background:"red"}}>Here is the example of Dynamic context, I send the the movie data from MovieContext to MovieList, and render the Data's </h1>
 
       <MovieProvider>
         <MovieList/>
--- src/DynamicContext/MovieList.jsx
+++ src/DynamicContext/MovieList.jsx
@@ -2,16 +2,18 @@ import React, { useContext } from 'react'
 import MovieContext from './MovieContext'
 
 const MovieList = () => {
-    const{movies} = useContext(MovieContext);
+  const { movies } = useContext(MovieContext);
   return (
-    <div>
-        {movies.map(item=>(
-            <div>
-                <h1>{item.name}</h1>
-                <img src={item.image} alt={item.name} width={"200px"}/>
-                <p>Rating: {item.rating}</p>
-            </div>
-        ))}
+    <div className='container'>
+      {movies.map(item => (
+        <div className="main-section">
+          <div className="section">
+            <h1>{item.name}</h1>
+            <img src={item.image} alt={item.name} width={"200px"} />
+            <p>Rating: {item.rating}</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
